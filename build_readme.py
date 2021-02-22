2adcfda7cc0b8150db7078de65f22b2b6b83ae9a
eugeneyan <eugeneyanziyou@gmail.com>
2021-02-21 16:40:38 -0800

    Add post count

--- build_readme.py
+++ build_readme.py
@@ -5,7 +5,7 @@ import re
 root = pathlib.Path(__file__).parent.resolve()
 
 
-def replace_chunk(content, marker, chunk, inline=False):
+def replace_writing(content, marker, chunk, inline=False):
     r = re.compile(
         r'<!\-\- {} starts \-\->.*<!\-\- {} ends \-\->'.format(marker, marker),
         re.DOTALL,
@@ -17,24 +17,29 @@ def replace_chunk(content, marker, chunk, inline=False):
 
 
 def fetch_writing():
-    entries = feedparser.parse('https://eugeneyan.com/feed.xml')['entries'][:5]
+    entries = feedparser.parse('https://eugeneyan.com/feed.xml')['entries']
+    top5_entries = entries[:5]
+    entry_count = len(entries)
     return [
-        {
-            'title': entry['title'],
-            'url': entry['link'].split('#')[0],
-            'published': re.findall(r'(.*?)\s00:00', entry['published'])[0]
-        }
-        for entry in entries
-    ]
+               {
+                   'title': entry['title'],
+                   'url': entry['link'].split('#')[0],
+                   'published': re.findall(r'(.*?)\s00:00', entry['published'])[0]
+               }
+               for entry in top5_entries
+           ], entry_count
 
 
 if __name__ == '__main__':
     readme_path = root / 'README.md'
     readme = readme_path.open().read()
-    entries = fetch_writing()[:5]
+    entries, entry_count = fetch_writing()
+    print(f'Recent 5: {entries}, Total count: {entry_count}')
     entries_md = '\n'.join(
         ['* [{title}]({url}) - {published}'.format(**entry) for entry in entries]
     )
-    rewritten = replace_chunk(readme, 'writing', entries_md)
+    rewritten_entries = replace_writing(readme, 'writing', entries_md)
+    readme_path.open('w').write(rewritten_entries)
 
-    readme_path.open('w').write(rewritten)
+    rewritten_entries = replace_writing(readme, 'writing_count', entry_count, inline=True)
+    readme_path.open('w').write(rewritten_entries)
